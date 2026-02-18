98bc9daf45d1e2787aeca33796e60952aa440ff0
fayendoumbe883-pixel <fayendoumbe883@gmail.com>
2026-02-18 00:04:09 +0000

    Add files via upload

---
+++ cv1.py
@@ -0,0 +1,43 @@
+import streamlit as st
+
+with st.sidebar:
+    st.title("👩🏽‍💼 FAYE")
+    st.title("👤 Ndoumbé Faye")
+    st.write("📍 Adresse:Dakar,Hann bel air,Sénégal")
+    st.write("📞 Tel;+221 78 593 52 22")
+    st.write("📧 Email:fayendoumbe883@gmail.com")
+    st.write("🎓 Année 2026-2027:BTS")
+    st.write("🎓 Année 2023-2024:BAC")
+    st.write("🌍 Langue:Francais")
+
+# Contenu principal
+
+st.header("👤 Profil")
+
+st.write("✨ Attentive,motivée,sérieuse,travailleuse:je suis à la recherche de stage afin de mettre en pratique mes compétences théoriques et de renforcer mes autres compétences.Correcte,rigoureuse et dynamique:je suis dotée de bonnes capacités d'analyse, d'un bon esprit d'apprentissage et de collaboration,Ainsi d'une excellente capacité d'integration.Mon but est de contribuer au développement de votre structure tout en recevant une expérience professionnelle")
+
+st.header("🗺 competences en géomatique")
+
+st.write("🛰 principes fondamentaux des Systémes d'Information Géographique")
+st.write("📊 collecte,structuration et actualisation des données géographiques")
+st.write("🗺 conception et consultation de cartes thématiques")
+st.write("📐 Bases de la topographie et des techniques de levés")
+st.write("💻 Traitement et analyse des données spatiales")
+st.write("🛰 Présentation de la télédétection et des images satellitaires")
+st.write("🌐 Notions de géodésie et des systemes de références et de coordonnées")
+st.write("🐍 Notions sur python:streamlit")
+
+st.header("🎓 competences en licence Géographie")
+
+st.write("🌍 Analyse des dynamiques environnementales et territoriales")
+st.write("📏 capacite à tracer un profil topographie")
+st.write("🗺 lecture et interprétation de cartes thématiques")
+
+st.header("💡 Compétences personnelles")
+
+st.write("🤝 capacité à travailler en équipe lors des travaux pratiques et sorties de terrain")
+st.write("⚖ respect de l'éthique et du travail collaboratif")
+st.write("🗣 capacité de communication orale et écrite")
+st.write("🔎 sens de l'observation et attention aux détails")
+st.write("⏳ Autonomie dans le travail et la gestion du temps")
+st.write("📋 respect des consignes au sein du structure")
